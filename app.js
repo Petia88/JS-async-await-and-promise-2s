01e90091b4a0d836ed6c6974dce0fa0de7c97975
Petia Mladenova <135381775+Petia88@users.noreply.github.com>
2024-05-30 21:08:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-async function throttlePromises() {
-    const asyncTasks = [
-        () => new Promise(resolve => setTimeout(() => {console.log('Task 1 is done.'); resolve('Task 1 done.')}, 1000)),
-        () => new Promise(resolve => setTimeout(() => {console.log('Task 2 is done.'); resolve('Task 2 done.')}, 1500)),
-        () => new Promise(resolve => setTimeout(() => {console.log('Task 3 is done.'); resolve('Task 3 done.')}, 500)),
-        () => new Promise(resolve => setTimeout(() => {console.log('Task 4 is done.'); resolve('Task 14 done.')}, 2000)),
-    ];
-
-    async function throttle(tasks, lmit) {
-        const results = [];
-        const executing = [];
-        for(const task of tasks) {
-            const p = task().then(result =>{
-                executing.splice(executing.indexOf(p), 1);
-                return result;
-            });
-            executing.push[p];
-            results.push[p];
-            if(executing.length >= lmit) {
-                await Promise.race(executing);
-            }
-        }
-
-        return Promise.all(results);
+async function fetchWithTimeout(url = "https://swapi.dev/api/people/1", timeout = 2000) {
+    try {
+        const response = await Promise.race([
+            fetch(url).then(response => response.json()),
+            new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout')), timeout))
+        ]);
+        console.log(response)
+    } catch (error) {
+        console.log(error.message)
     }
-
-    const results = await throttle(asyncTasks, 2);
-    console.log('All tasks are done.', results);
 }
