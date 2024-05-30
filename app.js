fecf79069dd47a7ba1e3f8ff930754e03dea61cc
Petia Mladenova <135381775+Petia88@users.noreply.github.com>
2024-05-30 21:08:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-async function retryPromise(promiseFunc, retries = 3) {
-  for(let i = 0; i < retries; i++){
-    try {
-      const result = await promiseFunc();
-      return result;
-    } catch (error) {
-      if(i === retries - 1) throw error;
+async function throttlePromises() {
+    const asyncTasks = [
+        () => new Promise(resolve => setTimeout(() => {console.log('Task 1 is done.'); resolve('Task 1 done.')}, 1000)),
+        () => new Promise(resolve => setTimeout(() => {console.log('Task 2 is done.'); resolve('Task 2 done.')}, 1500)),
+        () => new Promise(resolve => setTimeout(() => {console.log('Task 3 is done.'); resolve('Task 3 done.')}, 500)),
+        () => new Promise(resolve => setTimeout(() => {console.log('Task 4 is done.'); resolve('Task 14 done.')}, 2000)),
+    ];
+
+    async function throttle(tasks, lmit) {
+        const results = [];
+        const executing = [];
+        for(const task of tasks) {
+            const p = task().then(result =>{
+                executing.splice(executing.indexOf(p), 1);
+                return result;
+            });
+            executing.push[p];
+            results.push[p];
+            if(executing.length >= lmit) {
+                await Promise.race(executing);
+            }
+        }
+
+        return Promise.all(results);
     }
-  }
-}
 
-const unstablePromise = () => new Promise((resolve, reject) => {
-  Math.random() > 0.5 ? resolve('Success!') : reject(new Error('Failed!'));
-});
-
-function startRetry() {
-  retryPromise(unstablePromise)
-  .then(result => console.log(`Result: ${result}`))
-  .catch(error => console.log(`Error: ${error.message}`))
+    const results = await throttle(asyncTasks, 2);
+    console.log('All tasks are done.', results);
 }
