d3b7a7dfac22d4445fa07690f0db43aad4bad227
Petia Mladenova <135381775+Petia88@users.noreply.github.com>
2024-05-30 21:09:16 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-class AsyncQueue {
-    constructor() {
-        this.queue = [];
-        this.processing = false;
-    }
 
-    enque(task) {
-        this.queue.push(task);
-        if(!this.processing) this.process();
+function asyncGenerator(generatorFunc) {
+    const generator = generatorFunc();
+    function handle(result) {
+        if(result.done) return Promise.resolve(result.value);
+        return Promise.resolve(result.value).then(
+            res => handle(generator.next(res)),
+            err => handle(generator.throw(err))
+        )
     }
-
-    async process() {
-        this.processing = true;
-        while(this.queue.length > 0) {
-            const task = this.queue.shift();
-            await task();
-        }
-        this.processing = false;
+    try {
+        return handle(generator.next());
+    } catch (error) {
+        
     }
-
 }
 
-function startQueue() {
-    const queue = new AsyncQueue();
-    queue.enque(() => new Promise(resolve => setTimeout(() => {console.log('Task 1 is done'); resolve()}, 1000)));
-    queue.enque(() => new Promise(resolve => setTimeout(() => {console.log('Task 2 is done'); resolve()}, 500)));
-    queue.enque(() => new Promise(resolve => setTimeout(() => {console.log('Task 3 is done'); resolve()}, 1500)));
+function startAsyncGenerator() {
+   asyncGenerator(function*() {
+    const data1 = yield new Promise(resolve => setTimeout(() => resolve('Task 1 is done.'), 1000));
+    console.log(data1);
+    const data2 = yield new Promise(resolve => setTimeout(() => resolve('Task 2 is done.'), 1000));
+    console.log(data2);
+    const data3 = yield new Promise(resolve => setTimeout(() => resolve('Task 3 is done.'), 1000));
+    console.log(data3);
+   }
+
+)
 }
